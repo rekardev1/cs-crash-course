19ae97a194f0f91290d0c0e02f80aca361ad19dc
rekardev1 <rekardev1@gmail.com>
2023-09-20 19:44:28 +0300

    Commit

--- cs-crash-course/Program.cs
+++ cs-crash-course/Program.cs
@@ -1,3 +1,26 @@
 ﻿
+int number;
 
-Console.WriteLine("Hello!!!");
+number = 5;
+
+int number2 = 6;
+float f = 6.5f;
+
+Console.WriteLine(number);
+
+Console.WriteLine(number2);
+
+double number3 = 7.3;
+
+Console.WriteLine(number3);
+
+// number types: int, long, double, float
+// sizes          4     8     8        4
+
+string text = "Text";
+
+char myLetter = 'A';
+
+bool x = false;
+
+object obj = 'X';
